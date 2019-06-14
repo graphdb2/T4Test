9746c903242bce3d9c9bc1c514b17b556dfedfd6
Le Manach, Patrice (Technology, NatWest Markets) <patrice.lemanach@natwestmarkets.com>
2019-06-14 10:33:52 +0100

    First commit

---
+++ Depend/EntityAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace T4Test
+{
+	[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+	public class EntityAttribute : System.Attribute
+	{
+		public EntityAttribute(
+			string entityName,
+			string entityKey,
+			string[] stateKeyFields,
+			string targetLabel,
+			string targetStateLabel,
+			string stateLinkName,
+			string timeStampField = "from")
+		{
+			EntityName = entityName;
+			EntityKey = entityKey;
+			StateKeyFields = stateKeyFields;
+			TargetLabel = targetLabel;
+			TargetStateLabel = targetStateLabel;
+			StateLinkName = stateLinkName;
+			TimeStampField = timeStampField;
+		}
+
+		public virtual string EntityName { get; }
+
+		public virtual string EntityKey { get; }
+
+		public virtual string[] StateKeyFields { get; }
+
+		public string TargetLabel { get; }
+
+		public string TargetStateLabel { get; }
+
+		public string StateLinkName { get; }
+
+		public string TimeStampField { get; }
+	}
+}
---
+++ T4Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace T4Test
+{
+	public class Class1
+	{
+	}
+}
